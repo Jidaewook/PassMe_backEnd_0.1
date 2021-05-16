c56c4a1dce10681e2c488c70a0859f4817ceff02
Jidaewook <psatdoctor@naver.com>
2021-05-16 10:36:13 +0900

    Add middleware

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     res.json({
