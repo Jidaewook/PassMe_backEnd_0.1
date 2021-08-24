89429c97e99fc11f2bcd5073f3d0077384074845
Jidaewook <psatdoctor@naver.com>
2021-08-24 21:06:13 +0900

    UpdateRecover Workbook2

--- model/workbookModel.js
+++ model/workbookModel.js
@@ -1,35 +1,35 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const workbookSchema = new mongoose.Schema(
-    {
-        title: {
-            type: String,
-            required: true
-        },
-        desc: {
-            type: String,
-            required: true
-        },
-        genres_ids: [String],
-        rating: {
-            type: Number
-        },
-        poster: {
-            type: String
-        },
-        backdrop: {
-            type: String
-        },
-        professor: {},
-        comment: [],
-        like: [],
-        url: {
-            type: String
-        }
-    },
-    {
-        timestamps: true
-    }
-);
+// const workbookSchema = new mongoose.Schema(
+//     {
+//         title: {
+//             type: String,
+//             required: true
+//         },
+//         desc: {
+//             type: String,
+//             required: true
+//         },
+//         genres_ids: [String],
+//         rating: {
+//             type: Number
+//         },
+//         poster: {
+//             type: String
+//         },
+//         backdrop: {
+//             type: String
+//         },
+//         professor: {},
+//         comment: [],
+//         like: [],
+//         url: {
+//             type: String
+//         }
+//     },
+//     {
+//         timestamps: true
+//     }
+// );
 
-module.exports = mongoose.model('workbook', workbookSchema);
+// module.exports = mongoose.model('workbook', workbookSchema);
