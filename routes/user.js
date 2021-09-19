33db0849362fff1cf48aa67c30fcc82b5d28828d
Jidaewook <psatdoctor@naver.com>
2021-09-19 14:21:25 +0900

    Update valitation/bctypt

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const userModel = require('../model/userModel');
-
 const bctypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const userModel = require('../model/userModel');
+
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // register
 // @route POST users/register
@@ -12,10 +14,19 @@ const jwt = require('jsonwebtoken');
 // @access public 
 
 router.post('/register', (req, res) => {
-    const { name, email, password } = req.body;
-    
+    const {errors, isValid} = validateRegisterInput(req.body);
+
+// check validation
+    if(!isValid) {
+        return res.status(400).json(erros);
+    }
+
+    const email = req.body.email;
+    const name = req.body.name;
+    const password = req.body.password;
+
     userModel
-        .findOne({email})
+        .findOne({email : req.body.email})
         .then(user => {
             if(user) {
                 return res.status(404).json({
@@ -23,10 +34,23 @@ router.post('/register', (req, res) => {
                 });
             }
             const newUser = new userModel({
-                name, email, password
-            })
-            newUser.save().then(user => {
-                res.json(user)
+                name: req.body.name,
+                email: req.body.email, 
+                password: req.body.password
+            });
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if(err) throw err;
+                    newUser.password = hash;
+                    newUser.save()
+                            .then(user => {
+                                res.json(user)
+                            })
+                            .catch(err => {
+                                res.json(err)
+                            });
+                })
+            
             })
         })
         .catch(err => {
@@ -38,8 +62,15 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
     const { email, password } = req.body;
+    const { errors, isValid} = validateLoginInput(req.body);
 
-    console.log(req.body);
+    if(!isValid) {
+        return res.status(400).json(erros);
+    }
+
+    const email = req.body.email;
+    const password = req.body.password;
+
     userModel
         .findOne({email})
         .then(user => {
@@ -48,20 +79,53 @@ router.post('/login', (req, res) => {
                     error: 'user not found'
                 });
             }
-            user.comparePassword(password, (err, isMatch) => {
-                if (err || isMatch === false) {
-                    return res.status(400).json({
-                        err: "wrong password"
-                    });
-                }
-                const payload = { id: user._id, name: user.name, email: user.email, avatar: user.avatar};
+            bcrypt
+                .compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        //User Matched
+                        const payload = {
+                            id: user._id,
+                            name: user.name,
+                            email: user.email,
+                            avatar: user.avatar
+                        };
 
-                res.status(200).json({
-                    success: isMatch,
-                    token: tokenGenerator(payload)
-                });
+                        //Sign Token
+                        jwt.sign(
+                            payload,
+                            keys.secretOrKey,
+                            { expiresIn: 3600 },
+                            (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: 'Bearer ' + token
+                                });
+                            }
+                        );
+                    } else {
+                        errors.password = 'Password incorrect';
+                        return res.status(400).json(errors);
+
+                    }
+                })
+                .catch(err => res.json(err));
+
+
+            // user.comparePassword(password, (err, isMatch) => {
+            //     if (err || isMatch === false) {
+            //         return res.status(400).json({
+            //             err: "wrong password"
+            //         });
+            //     }
+            //     const payload = { id: user._id, name: user.name, email: user.email, avatar: user.avatar};
+
+            //     res.status(200).json({
+            //         success: isMatch,
+            //         token: tokenGenerator(payload)
+            //     });
                 
-            })
+            // })
         });
 });
 
